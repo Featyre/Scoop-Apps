96fa0dff80d0514536cf07663a9b5989a96b3a79
Featyre <featyre.cyyss@aleeas.com>
2022-08-31 17:08:47 +0800

    convert main to master

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "Featyre/Scoop-BleedingEdge:main"
+    [String]$upstream = "Featyre/Scoop-BleedingEdge:master"
 )
 
 if(!$env:SCOOP_HOME) { $env:SCOOP_HOME = Resolve-Path (scoop prefix scoop) }
